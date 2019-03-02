dc6cdfa5c6fb659c690b4734109ffe2b5fac8f28
convivial-system-4430 <cdneal091@gmail.com>
2019-03-02 16:45:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  []
+end
+
+def my_two_array
+  ["yeet","yeeted"]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
